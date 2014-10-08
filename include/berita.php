4574d91ee6219ff0d729c0f93b77832960c86abc
arvinnizar <nizar_arvin@yahoo.com>
2014-10-08 09:53:29 +0700

    add icon home

--- include/berita.php
+++ include/berita.php
@@ -1,42 +1,41 @@
+<?php
+session_start();
+include('include/config.inc');
+include('include/fungsi.inc');
+$link=mysql_connect($server,$user,$pass);
+mysql_select_db($db);
+?>
 <h4 class='title'>SISTEM INFORMASI MANAJEMEN KEPEGAWAIAN <?=$nama_unit_kerja?></h4>
-<table border="0" cellpadding="0" cellspacing="0" id="AutoNumber4">
-    <tr>
-      <td width="100%"><p>Selamat Datang di Portal Kepegawaian <?=$KAB?>. Berikut adalah fasilitas utama situs
-        ini.
-        </p>
-        <ul>
-        <li><a href="index.htm?sid=<?=$sid?>&do=biodata">Edit Biodata PNS</a>
-        </li>
-        <br>
-	Fasilitas ini digunakan untuk melakukan melihat dan update FIP.
-        <li><a href="index.htm?sid=<?=$sid?>&do=cari">Pencarian PNS</a>
-        </li>
-        <br>
-	Digunakan untuk melakukan pencarian PNS berdasarkan NIP, nama, dan/atau unit kerja.
-      </ul>
-      <p>Selain itu terdapat fasilitas-fasilitas tambahan.<ul>
-        <li><a href="index.htm?sid=<?=$sid?>&do=rekap">Rekap</a>
-        </li>
-        <br>
-	Rekap data dalam bentuk grafis berdasarkan golongan, pendidikan struktural, pendidikan umum, eselon,
-        agama, jenis kelamin, dan usia dikelompokkan berdasarkan unik kerja.
-        <li><a href="index.htm?sid=<?=$sid?>&do=struktur">Struktur Organisasi</a>
-        </li>
-        <br>
-	Menampilkan struktur organisasi tiap-tiap unit kerja.
-        <li><a href="index.htm?sid=<?=$sid?>&do=duk">Daftar Urut kepangkatan</a>
-        </li>
-        <br>
-	Melihat dan mencetak Daftar Urut Kepangkatan.
-        <li><a href="index.htm?sid=<?=$sid?>&do=nominatif">Nominatif</a>
-        </li>
-        <br>
-	Melihat dan mencetak nominatif PNS berdasarkan golongan, eselon, jenis kelamin, dan unit kerja.
-        <li><a href="index.htm?sid=<?=$sid?>&do=pensiun">Pensiun</a>
-        </li>
-        <br>
-	Mencari PNS yang akan pensiun dalam beberapa tahun ke depan berdasarkan unit kerja.
-      </ul>
-      <p>&nbsp;</td>
-    </tr>
-  </table>
+<?php
+$query = "select m.* from group_users g 
+        join grant_privileges gp on (g.id = gp.id_group_users)
+        join privileges p on (gp.id_privileges = p.id)
+        join module m on (p.id_module = m.id)
+        where gp.id_group_users = '".$_SESSION['group_user']."' group by m.id
+        ";
+
+$sql = mysql_query($query); ?>
+<table>
+<?php
+        while($data = mysql_fetch_array($sql)) { ?>
+        <tr><td><h5><?= $data['keterangan'] ?></h5></td></tr>
+        <tr><td>
+        <?php
+        $sql2 = mysql_query("select p.* from group_users g 
+            join grant_privileges gp on (g.id = gp.id_group_users)
+            join privileges p on (gp.id_privileges = p.id)
+            join module m on (p.id_module = m.id)
+            where gp.id_group_users = '".$_SESSION['group_user']."' and m.id = '".$data['id']."'");
+            while ($data2 = mysql_fetch_array($sql2)) { ?>
+                <div class="wrapp-icon" onclick="location.href='<?= $data2['url'] ?><?= $_GET['sid'] ?>'">
+                    <div class="wrapp-image">
+                        <img src="./images/cpanel/home/<?= $data2['icon'] ?>" />
+                    </div>
+                    <div class="wrapp-keterangan">
+                        <b><?= $data2['menu'] ?></b><br/><br/><p>Rekapitulasi Berdasar Golongan PNS</p>
+                    </div>
+                </div>
+            <?php } ?>
+            </td></tr>
+<?php } ?>
+</table>
