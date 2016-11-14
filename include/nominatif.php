0a973b665a8475e34c8dc2c1001b5621026930a0
Arvin Nizar <arvin.nizars@gmail.com>
2016-11-15 05:50:04 +0700

    update nominatif

--- include/nominatif.php
+++ include/nominatif.php
@@ -305,9 +305,9 @@ if (mysql_num_rows($rcu)>1) { $hasupt=true; }
                           <td width="610" align="left">
                             <select name="jabatan" id="jabatan" class="form-control" style="width: 300px;">
                               <option value="all" <? if ($jabatan=='all') echo "selected" ; ?>>Semua ...</option>
-                              <option value="0" <? if ($jabatan=='0') echo "selected" ; ?>>Staff</option>
+                              <option value="0" <? if ($jabatan=='0') echo "selected" ; ?>>Staff Umum</option>
                               <option value="1" <? if ($jabatan=='1') echo "selected" ; ?>>Struktural</option>
-                              <option value="2" <? if ($jabatan=='2') echo "selected" ; ?>>Fungsional</option>
+                              <option value="2" <? if ($jabatan=='2') echo "selected" ; ?>>Staff Khusus</option>
                             </select>
                           </td>
                         </tr>
